f840c02581c5159cadf7f0222fd81c124faa0837
YOSHIDA Hiroki <hyoshida@appirits.com>
2016-01-05 15:26:16 +0900

    Support .xls file

--- xlsx-textconv.rb
+++ xlsx-textconv.rb
@@ -1,3 +1,31 @@
 #!/usr/bin/env ruby
 require 'roo'
-Roo::Excelx.new(ARGV[0]).each_with_pagename {|name, sheet| puts name; puts sheet.to_yaml}
+
+class XlsxTextconv
+  class << self
+    def run(filepath)
+      open(filepath).each_with_pagename {|name, sheet| puts name; puts sheet.to_yaml}
+    end
+
+    private
+
+    def open(filepath)
+      klass = class_for(filepath)
+      klass.new(filepath)
+    end
+
+    def class_for(fillepath)
+      case File.extname(fillepath)
+      when '.xls'
+        require 'roo-xls'
+        Roo::Excel
+      when '.xlsx'
+        Roo::Excelx
+      else
+        fail "Can't open the type of file: #{fillepath}"
+      end
+    end
+  end
+end
+
+XlsxTextconv.run(ARGV[0])
